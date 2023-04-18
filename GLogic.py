dfb4a2245fc55f8cc993ceff5cfd6c8a6f06395e
SoulOfHope <97285437+SoulOfHope@users.noreply.github.com>
2023-04-18 13:09:11 +0100

    Create GLogic.py
    Prep work

---
+++ GLogic.py
@@ -0,0 +1,11 @@
+#This is Breaker GLogic, for Gate Logic, this will allow quick access to AND OR NOR NOT and XOR gates (More Gates will be added if needed)
+
+# AND GATE
+
+# OR GATE
+
+# NOR GATE
+
+# XOR GATE
+
+# NOT GATE
